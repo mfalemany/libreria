c78cc499148f53ad296225d28661a2280719aaf0
root <root@debian>
2013-08-31 15:51:33 -0300

    Iniciando Repositorio

---
+++ trunk/application/controllers/inicio.php
@@ -0,0 +1,19 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Inicio extends CI_Controller{
+	
+	function __construct(){
+		parent::__construct();
+	}
+
+	function index(){
+		$this->load->view("include/header");
+		$this->load->view("inicio_view");
+		$this->load->view("include/footer");
+	
+	}
+
+}
+
+/* End of file inicio.php */
+/* Location: ./application/controllers/inicio.php */
